f3a33ddcc60b3bb9080931aab02c64fd1e221cfb
Джейхун Ахмедов <lezgin5.5@mail.ru>
2020-09-27 20:45:25 +0300

    Добавьте файлы проекта.

---
+++ WcfServiceLibraryTest/IService1.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfServiceLibraryTest
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени интерфейса "IService1" в коде и файле конфигурации.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json)] 
+        string GetData(string value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Добавьте здесь операции служб
+    }
+
+    // Используйте контракт данных, как показано на следующем примере, чтобы добавить сложные типы к сервисным операциям.
+    // В проект можно добавлять XSD-файлы. После построения проекта вы можете напрямую использовать в нем определенные типы данных с пространством имен "WcfServiceLibraryTest.ContractType".
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ WcfServiceLibraryTest/Service1.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceLibraryTest
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "Service1" в коде и файле конфигурации.
+    public class Service1 : IService1
+    {
+        public string GetData(string value)
+        {
+            var result = Math.Pow(int.Parse(value), 2);
+            if (int.Parse(value) % 2 == 0)
+            {
+                string str = $"{{ \"number\" : {result.ToString()} }}";
+                return str;
+            }
+            else
+            {
+                return "Неверное значение";
+            }
+        }
+
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
